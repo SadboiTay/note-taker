8ec87e60c9c132d775fa2dace046c2a5cd1253c9
SadboiTay <tg.hakes@gmail.com>
2021-09-30 16:31:53 -0600

    set up html and api routes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// parce incoming string or array data
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 // unlock static files from behind the gates
 app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
