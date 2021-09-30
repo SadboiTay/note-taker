2d23d7c222b99bdb9783176cf615317f7a381490
SadboiTay <tg.hakes@gmail.com>
2021-09-30 16:12:38 -0600

    starter-code and initial server.js setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// parce incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// unlock static files from behind the gates
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
