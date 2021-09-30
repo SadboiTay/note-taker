828276d7ff25169f61c639c2417b81f435c2866f
SadboiTay <tg.hakes@gmail.com>
2021-09-30 16:52:59 -0600

    add api POST route for notes, addNewNote() added to handle req

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
-const notes = require("../../db/db");
+const notes = require("../../db/db.json");
+const addNewNote = require("../../lib/notes");
 
 router.get("/notes", (req, res) => {
     res.json(notes);
 });
+
+router.post('/notes', (req, res) => {
+    const note = addNewNote(req.body, notes);
+    res.json(note);
+})
 
 module.exports = router;
