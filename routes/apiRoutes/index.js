30b8d4b8b2aecce4a3c4a23a88237a33ef3af82f
SadboiTay <tg.hakes@gmail.com>
2021-10-01 16:12:06 -0600

    delete route and getNotIndex() built

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const notes = require("../../db/db.json");
-const addNewNote = require("../../lib/notes");
+const { addNewNote, getNoteIndex } = require("../../lib/notes");
+const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
     res.json(notes);
@@ -9,6 +11,20 @@ router.get("/notes", (req, res) => {
 router.post('/notes', (req, res) => {
     const note = addNewNote(req.body, notes);
     res.json(note);
+})
+
+router.delete('/notes/:id', (req, res) => {
+    const deleteId = parseInt(req.params.id);
+    const deleteIndex = getNoteIndex(deleteId, notes);
+
+    notes.splice(deleteIndex, 1);
+
+    res.send(notes);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(notes, null, 2)
+    );
 })
 
 module.exports = router;
