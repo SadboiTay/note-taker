0c442f5c784d8e0052dc159697277b99f802623a
SadboiTay <tg.hakes@gmail.com>
2021-10-01 16:56:46 -0600

    test entries

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -19,12 +19,12 @@ router.delete('/notes/:id', (req, res) => {
 
     notes.splice(deleteIndex, 1);
 
-    res.send(notes);
-
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
         JSON.stringify(notes, null, 2)
     );
+    
+    res.send(notes);
 })
 
 module.exports = router;
